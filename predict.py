357986028f23a7ee2ab8fc408be4fdb96b848713
varsanyim <6471221+varsanyim@users.noreply.github.com>
2025-05-24 18:35:44 +0200

    works

--- predict.py
+++ predict.py
@@ -1,5 +1,35 @@
 from common import *
 
+def process_image(image):
+    ''' Scales, crops, and normalizes a PIL image for a PyTorch model,
+        returns an Numpy array
+    '''
+    image = Image.open(image).convert("RGB")
+
+    # Resize keeping aspect ratio so that shortest side = 256
+    aspect = image.width / image.height
+    if image.width < image.height:
+        image = image.resize((256, int(256 / aspect)))
+    else:
+        image = image.resize((int(256 * aspect), 256))
+
+    # Center crop 224x224
+    left = (image.width - 224) / 2
+    top = (image.height - 224) / 2
+    right = left + 224
+    bottom = top + 224
+    image = image.crop((left, top, right, bottom))
+
+    # Convert to NumPy and normalize
+    np_image = np.array(image) / 255.0
+    mean = np.array([0.485, 0.456, 0.406])
+    std = np.array([0.229, 0.224, 0.225])
+    np_image = (np_image - mean) / std
+
+    # Reorder dimensions
+    np_image = np_image.transpose((2, 0, 1))
+    return torch.from_numpy(np_image).type(torch.FloatTensor)
+
 def predict(image_path, model, device, topk):
     model.eval()
     
@@ -53,7 +83,7 @@ if __name__ == '__main__':
     parser.add_argument('image_path', type=str, help='Image Path')
     parser.add_argument('checkpoint', type=str, default=".", help='Checkpoint directory (filename is checkpoint.pth)')
     parser.add_argument('--top_k', type=int, default=5, help='Return top K most likely classes')
-    parser.add_argument('--category_names', type=str, default='cat_to_name.json' help='cat_to_name.json file')
+    parser.add_argument('--category_names', type=str, default='cat_to_name.json', help='cat_to_name.json file')
     parser.add_argument('--gpu', action='store_true', help='Use GPU if available')
 
     args = parser.parse_args()
